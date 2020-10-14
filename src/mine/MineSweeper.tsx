b28151a6f7f76da7166dbb97c76fa7abf5909765
Kevin <kevin.brereton@ucalgary.ca>
2020-10-14 11:41:21 -0600

    finished base version

--- src/mine/MineSweeper.tsx
+++ src/mine/MineSweeper.tsx
@@ -1,162 +1,176 @@
 import React, {useCallback, useEffect, useReducer, useRef, useState} from "react";
 import "./mine_sweeper.css"
-
-import {Button, Col, Container, Dropdown, Row} from "react-bootstrap"
+import clock from "./stopwatch.png"
+import flag from "./flag.png"
+import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
+import Dropdown from "react-bootstrap/Dropdown"
+import Row from "react-bootstrap/Row"
 import {SelectCallback} from "react-bootstrap/helpers";
 import {MineSweeperGame} from "./MiseSweeperGame";
 
-type MinePlayState="won"|"playing"|"lost";
-type DifficultyStr="easy"|"medium"|"hard";
-const EASY_STR:DifficultyStr="easy";
-const MEDIUM_STR:DifficultyStr="medium";
-const HARD_STR:DifficultyStr="hard";
-export interface DifficultySettings{
-    flags:number;
-    cols:number;
-    rows:number;
-    str:DifficultyStr;
+type MinePlayState = "won" | "playing" | "lost";
+type DifficultyStr = "easy" | "medium" | "hard";
+const EASY_STR: DifficultyStr = "easy";
+const MEDIUM_STR: DifficultyStr = "medium";
+const HARD_STR: DifficultyStr = "hard";
+
+export interface DifficultySettings {
+    flags: number;
+    cols: number;
+    rows: number;
+    str: DifficultyStr;
 }
 
-const difficultyToSettings={
-    easy:{flags:10,cols:10,rows:8,str:EASY_STR},
-    medium:{flags:40,cols:18,rows:14,str:MEDIUM_STR},
-    hard:{flags:99,cols:24,rows:20,str:HARD_STR},
+const difficultyToSettings = {
+    easy: {flags: 10, cols: 10, rows: 8, str: EASY_STR},
+    medium: {flags: 40, cols: 18, rows: 14, str: MEDIUM_STR},
+    hard: {flags: 99, cols: 24, rows: 20, str: HARD_STR},
 }
 
-interface NextOrSet{
-    mode:"increment"|"reset";
-    value:number;
+interface NextOrSet {
+    mode: "increment" | "reset";
+    value: number;
 }
-function incrementOrSet(state:number,next:NextOrSet){
-    if(next.mode === "increment") {
+
+function incrementOrSet(state: number, next: NextOrSet) {
+    if (next.mode === "increment") {
         return state + next.value;
-    }
-    else{
+    } else {
         return next.value
     }
 }
 
-function getBestTimeKey(difficulty:DifficultySettings){
-    return `${difficulty.str}_high_score`
+function getBestTimeKey(difficulty: DifficultySettings) {
+    return `${difficulty.str}_high_score`;
 }
 
-function MineSweeperMainBar({setDifficulty, difficulty, time, numFlags}:{setDifficulty:SelectCallback,difficulty:DifficultySettings,time:number, numFlags:number}){
-    return<Container fluid>
+function MineSweeperMainBar({setDifficulty, difficulty, time, numFlags}: { setDifficulty: SelectCallback, difficulty: DifficultySettings, time: number, numFlags: number }) {
+    return <Container fluid className={"mine-bar"}>
         <Row>
             <Col>
-                <Dropdown onSelect={setDifficulty}>
-                <Dropdown.Toggle>
-                    {difficulty.str}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item eventKey={EASY_STR} active={difficulty.str === EASY_STR}>
-                        easy
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey={MEDIUM_STR} active={difficulty.str === MEDIUM_STR}>
-                        medium
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey={HARD_STR} active={difficulty.str === HARD_STR}>
-                        hard
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+                <Dropdown onSelect={setDifficulty} className="bar-dropdown">
+                    <Dropdown.Toggle className="bar-dropdown-toggle" variant="success">
+                        {difficulty.str}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item eventKey={EASY_STR} active={difficulty.str === EASY_STR} className="bar-dropdown-item">
+                            easy
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey={MEDIUM_STR} active={difficulty.str === MEDIUM_STR} className="bar-dropdown-item">
+                            medium
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey={HARD_STR} active={difficulty.str === HARD_STR} className="bar-dropdown-item">
+                            hard
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Col>
             <Col>
+                <img className="bar-image" src={clock} alt="time"/>
                 {time}
             </Col>
             <Col>
+                <img className="bar-image" src={flag} alt="flag"/>
                 {numFlags}
             </Col>
         </Row>
     </Container>
 }
 
-function WinLossScreen({time,difficulty,onContinue, message}:{time:number|null,difficulty:DifficultySettings,onContinue:()=>void,message:string}){
+function WinLossScreen({time, difficulty, onContinue, message}: { time: number | null, difficulty: DifficultySettings, onContinue: () => void, message: string }) {
 
-    const timeDisplayed=time===null? "---":time.toString();
-    const bestTime=localStorage.getItem(getBestTimeKey(difficulty));
-    const bestTimeDisplayed=bestTime===null? "---":bestTime;
+    const timeDisplayed = time === null ? "---" : time.toString();
+    const bestTime = localStorage.getItem(getBestTimeKey(difficulty));
+    const bestTimeDisplayed = bestTime === null ? "---" : bestTime;
 
-    return<Container fluid>
-        <Row>
-            {message}
-        </Row>
+    return <Container fluid className="win-loss-screen">
         <Row>
             <Col>
-                {timeDisplayed}
-                best time:{bestTimeDisplayed}
+                {message}
             </Col>
         </Row>
         <Row>
-            <Button onClick={onContinue}>Continue...</Button>
+            <Col>
+                <div className="winloss-best-time-label">Time:</div>
+                <div className="winloss-best-time">{timeDisplayed}</div>
+            </Col>
+            <Col>
+                <div className="winloss-best-time-label">Best Time:</div>
+                <div className="winloss-best-time">{bestTimeDisplayed}</div>
+            </Col>
+        </Row>
+        <Row>
+            <Col className="button-extra-margin">
+                <Button onClick={onContinue}>Continue...</Button>
+            </Col>
         </Row>
     </Container>
 }
 
 
-
 //todo remove eslint settings.
 export function MineSweeper() {
-    const [difficulty,setDifficulty]=useReducer((state:DifficultySettings,next:DifficultyStr)=>difficultyToSettings[next],difficultyToSettings.easy);
-    const [timer,incrementOrSetTimerBy]=useReducer((state:number, next:NextOrSet={mode:"increment",value:1})=>incrementOrSet(state,next),0);
+    const [difficulty, setDifficulty] = useReducer((state: DifficultySettings, next: DifficultyStr) => difficultyToSettings[next], difficultyToSettings.easy);
+    const [timer, incrementOrSetTimerBy] = useReducer((state: number, next: NextOrSet = {mode: "increment", value: 1}) => incrementOrSet(state, next), 0);
 
-    const [numFlagsRemaining,decrementOrSetNumFlagsBy]=useReducer((state:number, next:NextOrSet={mode:"increment",value:-1})=>incrementOrSet(state,next),difficulty.flags);
+    const [numFlagsRemaining, decrementOrSetNumFlagsBy] = useReducer((state: number, next: NextOrSet = {
+        mode: "increment",
+        value: -1
+    }) => incrementOrSet(state, next), difficulty.flags);
     // @ts-ignore
-    const incrementNumFlags=useCallback(()=>decrementOrSetNumFlagsBy({mode:"increment",value:1}),[decrementOrSetNumFlagsBy])
+    const incrementNumFlags = useCallback(() => decrementOrSetNumFlagsBy({mode: "increment", value: 1}), [decrementOrSetNumFlagsBy])
 
-    const [playState,setPlayState]=useState<MinePlayState>("playing");
+    const [playState, setPlayState] = useState<MinePlayState>("playing");
 
-   const localsRef= useRef({timer,difficulty});
+    const localsRef = useRef({timer, difficulty});
 
-    const onWin=useCallback(()=>{
-        const {timer,difficulty}= localsRef.current;
-        const bestTimeKey=getBestTimeKey(difficulty);
-        const bestTime=localStorage.getItem(bestTimeKey);
-        if(bestTime === null || timer < parseInt(bestTime) ){
-            localStorage.setItem(bestTimeKey,timer.toString());
+    const onWin = useCallback(() => {
+        const {timer, difficulty} = localsRef.current;
+        const bestTimeKey = getBestTimeKey(difficulty);
+        const bestTime = localStorage.getItem(bestTimeKey);
+        if (bestTime === null || timer < parseInt(bestTime)) {
+            localStorage.setItem(bestTimeKey, timer.toString());
         }
         setPlayState("won")
-    },[setPlayState,localsRef])
+    }, [setPlayState, localsRef])
 
-    const onLoss=useCallback(()=>setPlayState("lost"),[setPlayState])
+    const onLoss = useCallback(() => setPlayState("lost"), [setPlayState])
 
     //when difficulty changes, reset timer back to 0 and set num flags remaining
-    useEffect(()=>{
-        let interval:any = null;
-        if(playState === "playing"){
-            interval=setInterval(incrementOrSetTimerBy,1000);
+    useEffect(() => {
+        let interval: any = null;
+        if (playState === "playing") {
+            interval = setInterval(incrementOrSetTimerBy, 1000);
             //useReducer currently bugged so thinks default parameters make it so no parameters can happen.
             // ommiting typescript ignores will make it so it thinks it has too many arguments, expected 0
             // @ts-ignore
-            incrementOrSetTimerBy({mode:"reset",value:0});
+            incrementOrSetTimerBy({mode: "reset", value: 0});
             // @ts-ignore
-            decrementOrSetNumFlagsBy({mode:"reset",value:difficulty.flags});
+            decrementOrSetNumFlagsBy({mode: "reset", value: difficulty.flags});
         }
-        return ()=>{
-            if (interval!==null){
+        return () => {
+            if (interval !== null) {
                 clearInterval(interval)
             }
         }
-    },[difficulty,incrementOrSetTimerBy,playState])
-
+    }, [difficulty, incrementOrSetTimerBy, playState])
 
 
     let content;
-    if (playState === "playing"){
-        const canPlaceFlags=numFlagsRemaining!==0;
-
-        content=<>
+    if (playState === "playing") {
+        content = <>
             <MineSweeperMainBar difficulty={difficulty} setDifficulty={setDifficulty as SelectCallback} numFlags={numFlagsRemaining} time={timer}/>
-            <MineSweeperGame difficulty={difficulty} canPlaceFlags={canPlaceFlags} decrementNumFlags={decrementOrSetNumFlagsBy} incrementNumFlags={incrementNumFlags} onLoss={onLoss} onWin={onWin}/>
+            <MineSweeperGame difficulty={difficulty} decrementNumFlags={decrementOrSetNumFlagsBy} incrementNumFlags={incrementNumFlags} onLoss={onLoss} onWin={onWin}/>
         </>
+    } else {
+        const timeOrNull = playState === "won" ? timer : null;
+        const message = playState === "won" ? "You Won!" : "Better Luck Next Time";
+        content = <WinLossScreen time={timeOrNull} difficulty={difficulty} message={message} onContinue={() => setPlayState("playing")}/>
     }
-    else{
-        const timeOrNull= playState === "won" ? timer:null;
-        const message= playState === "won" ? "you won!":"better luck next time bud";
-        content=<WinLossScreen time={timeOrNull} difficulty={difficulty} message={message} onContinue={()=>setPlayState("playing")}/>
-    }
-    localsRef.current={timer,difficulty}
-    return<div className={"minesweeper-div"}>
+    localsRef.current = {timer, difficulty}
+    return <div className={"minesweeper-div"}>
         {content}
     </div>
 }
--- src/mine/MiseSweeperGame.tsx
+++ src/mine/MiseSweeperGame.tsx
@@ -1,94 +1,101 @@
 import React, {memo, useEffect, useRef, useState} from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
 import {DifficultySettings} from "./MineSweeper";
 import {MineRender, MSGame} from "./GameEngine";
-import {render} from "react-dom";
 
-const RIGHT_CLICK_THRESHOLD_TIME_MS=1000;
+const RIGHT_CLICK_THRESHOLD_TIME_MS = 1000;
 
-function resetGame(difficulty:DifficultySettings,engineRef:any,setRendering:(v:Array<Array<MineRender>>)=>void ){
-    const {rows,cols,flags}=difficulty;
-    engineRef.current=new MSGame(rows,cols,flags);
+function resetGame(difficulty: DifficultySettings, engineRef: any, setRendering: (v: Array<Array<MineRender>>) => void) {
+    const {rows, cols, flags} = difficulty;
+    engineRef.current = new MSGame(rows, cols, flags);
     setRendering(engineRef.current.getRendering());
 }
 
-function MinePoint({flag,onClick,onRightAndLongClick}:{flag:MineRender,onClick:()=>void,onRightAndLongClick:()=>void}){
-    const timeStampRef=useRef<number|null>(null);
-    const isNumber=!isNaN(Number(flag))
-    const content = isNumber ? flag:"";
-    const divClass = isNumber ? "mine-number":`${flag}-card`;
+function MinePoint({flag, onClick, onRightAndLongClick}: { flag: MineRender, onClick: () => void, onRightAndLongClick: () => void }) {
+    const timeStampRef = useRef<number | null>(null);
+    const isNumber = !isNaN(Number(flag))
+    const divClass = isNumber ? "mine-number" : `${flag}-card`;
 
-    const onPointerLeave=()=>{timeStampRef.current=null}
-    const onPointerDown=(evt:React.PointerEvent)=>{
-        if(evt.button === 0){
+    const onPointerLeave = () => {
+        timeStampRef.current = null
+    }
+    const onPointerDown = (evt: React.PointerEvent) => {
+        if (evt.button === 0) {
             evt.preventDefault();
-            timeStampRef.current=evt.timeStamp;
+            timeStampRef.current = evt.timeStamp;
         }
     }
-    const onPointerUp=(evt:React.PointerEvent)=>{
-        if(evt.button === 0) {
+    const onPointerUp = (evt: React.PointerEvent) => {
+        if (evt.button === 0) {
+            evt.stopPropagation()
             evt.preventDefault();
-            if(timeStampRef.current!==null){
-                const timeMs= evt.timeStamp-timeStampRef.current;
-                if(timeMs > RIGHT_CLICK_THRESHOLD_TIME_MS){
+            if (timeStampRef.current !== null) {
+                const timeMs = evt.timeStamp - timeStampRef.current;
+                if (timeMs >= RIGHT_CLICK_THRESHOLD_TIME_MS) {
                     onRightAndLongClick();
-                }
-                else{
+                } else {
                     onClick();
                 }
             }
         }
     }
-    const onRightClick=(evt:React.MouseEvent)=>{
+    const onRightClick = (evt: React.MouseEvent) => {
         evt.preventDefault();
-        onRightAndLongClick();
+        if (evt.button === 2) {
+            onRightAndLongClick();
+        }
     }
-    return <div className={`grid-card ${divClass}`} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerLeave={onPointerLeave} onContextMenu={onRightClick}>{content}</div>
+    return <div className={`grid-card ${divClass}`} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerLeave={onPointerLeave}
+                onContextMenu={onRightClick}>{flag}</div>
 }
-export const MineSweeperGame = memo(({difficulty, canPlaceFlags, decrementNumFlags, incrementNumFlags, onWin, onLoss}: { difficulty: DifficultySettings, canPlaceFlags: boolean, decrementNumFlags: () => void, incrementNumFlags: () => void, onWin: () => void, onLoss: () => void }) => {
-    const engineRef=useRef(new MSGame(difficulty.rows,difficulty.cols,difficulty.flags));
-    const [rendering, setRendering] = useState<Array<Array<MineRender>>>(engineRef.current.getRendering);
-    useEffect(()=> resetGame(difficulty,engineRef,setRendering),[difficulty,engineRef,setRendering]);
 
-    const engine=engineRef.current
-    const {exploded,done}=engine.getStatus()
+export const MineSweeperGame = memo(({difficulty, decrementNumFlags, incrementNumFlags, onWin, onLoss}: { difficulty: DifficultySettings, decrementNumFlags: () => void, incrementNumFlags: () => void, onWin: () => void, onLoss: () => void }) => {
+    const engineRef = useRef(new MSGame(difficulty.rows, difficulty.cols, difficulty.flags));
+    const [rendering, setRendering] = useState<Array<Array<MineRender>>>(engineRef.current.getRendering);
+    useEffect(() => resetGame(difficulty, engineRef, setRendering), [difficulty, engineRef, setRendering]);
+
+    const engine = engineRef.current
+    const {exploded, done} = engine.getStatus()
     let dataPoints;
 
-    if(!done){
-        const onClick=(rowIndex:number,colIndex:number,flag:MineRender)=>{
+    if (!done) {
+        const onClick = (rowIndex: number, colIndex: number, flag: MineRender) => {
             //will not let you uncover a flagged square, like googles
-            if(flag === "H"){
-                engine.uncover(rowIndex,colIndex);
+            if (flag === "H") {
+                engine.uncover(rowIndex, colIndex);
                 setRendering(engine.getRendering());
             }
         }
-        const onRightClick=(rowIndex:number,colIndex:number,flag:MineRender)=>{
-            if(flag === "H" && canPlaceFlags){
-                engine.mark(rowIndex,colIndex);
+        const onRightClick = (rowIndex: number, colIndex: number, flag: MineRender) => {
+            if (flag === "H") {
+                engine.mark(rowIndex, colIndex);
                 setRendering(engine.getRendering())
                 decrementNumFlags();
-            }
-            else if(flag === "F"){
-                engine.mark(rowIndex,colIndex);
+            } else if (flag === "F") {
+                engine.mark(rowIndex, colIndex);
                 setRendering(engine.getRendering())
                 incrementNumFlags();
             }
         }
-        dataPoints=rendering.map((row,rowIndex)=>row.map((flag,colIndex)=>{
+        dataPoints = rendering.map((row, rowIndex) => row.map((flag, colIndex) => {
             return <MinePoint key={`game_mine_${rowIndex}_${colIndex}`} flag={flag}
-                              onClick={()=>onClick(rowIndex,colIndex,flag)} onRightAndLongClick={()=>onRightClick(rowIndex,colIndex,flag)}
+                              onClick={() => onClick(rowIndex, colIndex, flag)} onRightAndLongClick={() => onRightClick(rowIndex, colIndex, flag)}
             />
         }))
-    }
-    else{
-        const onNextClick=exploded? onLoss:onWin;
-        dataPoints=rendering.map((row,rowIndex)=>row.map((flag,colIndex)=>{
+    } else {
+        const onNextClick = exploded ? onLoss : onWin;
+        dataPoints = rendering.map((row, rowIndex) => row.map((flag, colIndex) => {
             return <MinePoint key={`game_mine_${rowIndex}_${colIndex}`} flag={flag} onClick={onNextClick} onRightAndLongClick={onNextClick}/>
         }))
     }
 
-    return<div className="grid" style={{gridTemplateColumns: `repeat(${difficulty.cols}, 1fr)`} as any}>
-            {dataPoints}
-        </div>
+
+    const {rows, cols} = difficulty
+    return <div className="grid" style={{
+        gridTemplateColumns: `repeat(${difficulty.cols}, ${100 / difficulty.cols}%)`,
+        gridTemplateRows: `repeat(${difficulty.rows}, ${100 / difficulty.rows}%)`,
+        height: `calc(${rows / cols}*min(800px,100vw, 100vh))`
+    } as any}>
+        {dataPoints}
+    </div>
 })
 
