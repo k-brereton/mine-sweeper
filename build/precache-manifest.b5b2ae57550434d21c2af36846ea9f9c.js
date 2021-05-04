7f8a44a7f301b33f5828d4ba745c725e10e82f1f
k-brereton <kevin.brereton@ucalgary.ca>
2021-05-04 14:31:53 -0600

    Fixed bug where github pages deployment used incorrect url

---
+++ build/precache-manifest.b5b2ae57550434d21c2af36846ea9f9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0ed6f985b5438cfa70382ba502ce13",
+    "url": "/mine-sweeper/build/index.html"
+  },
+  {
+    "revision": "8087c0b9b81dbaa0bc1d",
+    "url": "/mine-sweeper/build/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "50cd5ccacf0d9dc022d6",
+    "url": "/mine-sweeper/build/static/css/main.99e74253.chunk.css"
+  },
+  {
+    "revision": "8087c0b9b81dbaa0bc1d",
+    "url": "/mine-sweeper/build/static/js/2.917cb1c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mine-sweeper/build/static/js/2.917cb1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cd5ccacf0d9dc022d6",
+    "url": "/mine-sweeper/build/static/js/main.61ba5965.chunk.js"
+  },
+  {
+    "revision": "b572ad45aacc56de68f0",
+    "url": "/mine-sweeper/build/static/js/runtime-main.4606f2c2.js"
+  },
+  {
+    "revision": "329cda52fc348dd38744d6dfabce52f1",
+    "url": "/mine-sweeper/build/static/media/mine.329cda52.png"
+  },
+  {
+    "revision": "6d55fa6d7d9117c73a5cb633273fe7d3",
+    "url": "/mine-sweeper/build/static/media/stopwatch.6d55fa6d.png"
+  }
+]);
